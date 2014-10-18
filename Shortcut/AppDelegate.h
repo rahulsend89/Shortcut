acce8049bd468e14ad30c3fb7e5c25482a0e75bc
Rahul Malik <rahul.send89@gmail.com>
2014-10-18 22:45:50 +0530

    Initial Commit

---
+++ Shortcut/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Shortcut
+//
+//  Created by Rahul Malik on 18/10/14.
+//  Copyright (c) 2014 Rahul Malik. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Shortcut/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Shortcut
+//
+//  Created by Rahul Malik on 18/10/14.
+//  Copyright (c) 2014 Rahul Malik. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
